58d23d488a8eaef2b4ba1e4b9676744c42650642
Alejandro Botana Moreno <abotanam@gmail.com>
2021-11-29 17:53:35 +0100

    Test mac

--- app/Http/Controllers/CursosController.php
+++ app/Http/Controllers/CursosController.php
@@ -123,13 +123,13 @@ class CursosController extends Controller
             if($busqueda -> has('busqueda')){
 
                $cursos = Curso::select(['id','titulo','portada'])                        
-                        ->withCount('videos as cantidad_videos')    
+                        ->withCount('videos as videos_curso')    
                         ->where('titulo','like','%'. $busqueda -> input('busqueda').'%')
                         ->get();
 
             }else{
                 $cursos = Curso::select(['id','titulo','portada'])                        
-                        ->withCount('videos as cantidad_videos')    
+                        ->withCount('videos as videos_curso')    
                         ->get();
                
             }
